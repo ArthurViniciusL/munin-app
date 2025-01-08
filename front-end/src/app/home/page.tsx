62bb23d6f11ac1cc77b3e25d592c1de363274536
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2025-01-08 14:30:17 -0300

    get in backend

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -3,14 +3,13 @@ import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.box}>
+    <>
       <section className={styles.content}>
         <p className="">
-          Selecione a sua imagem gerada na <span className={styles.msg}>Arte de fio a pavio</span>.
-        </p>        
-        <InputPicture/>
+          Selecione a sua imagem gerada na <span className="art:ft:yellow-02 art:ft:bold-03">Arte de fio a pavio</span>.
+        </p>
+        <InputPicture />
       </section>
-
-    </div>
+    </>
   );
 }
