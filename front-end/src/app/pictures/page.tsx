6ad06ce11bf4aec2a9941816d8bf9214b259b55c
Arthur Lucena <arthurviniciussl.contato@gmail.com>
2025-02-12 13:44:56 -0300

    Restored the download screen components to make the application more componentized

--- front-end/src/app/pictures/page.tsx
+++ front-end/src/app/pictures/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import styles from "./pictures.module.css";
-import { use, useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { CardToDownload } from "@/components/CardToDownload";
-import { DownloadImageProvider } from "@/context/DownloadImageContext";
-import Image from "next/image";
-import marjo_tom from "@/assets/images/no_listed_images.svg";
-import { useUploadContext } from "@/context/UploadContext";
-
+import { NoImagesReceived } from "./components/NoImagesReceived";
 export default function Pictures() {
-  const { uploadIsConfirmed, setUploadIsConfirmed } = useUploadContext();
+
   const [dataState, setDataState] = useState([]);
 
   const fetchData = useCallback(async () => {
@@ -27,49 +23,26 @@ export default function Pictures() {
       console.error(error);
     }
   }, [])
-  /*
+
   useEffect(() => {
     fetchData();
-  },[uploadIsConfirmed])
-  */
-
-  // useEffect(() => {
-  // const interval = setInterval(fetchData, 1000);
-  // return () => clearInterval(interval);
-  // fetchData();
-  // }, [uploadIsConfirmed]);
+  }, [])
 
   return (
     <>
-      {
-        dataState.length > 0 ?
-          <section className={styles.content}>
-            <h1>
-              Lista de imagens:
-            </h1>
-            <CardToDownload pictures={dataState} />
-          </section>
-
-          :
-
-          <section className={styles.content}>
-            < div className={styles.messageBox}>
-
-              <h1 className={`
-              art:p-02
-              art:font:Caveat-Brush
-              art:font:title
-              art:font:yellow-01
-              mac:motion:ease-in-out-infinite:[Y:-30]
-              ${styles.msgNoImage}
-            `}>
-                Nenhuma imagem recebida.
+      <section className={styles.content}>
+        {
+          dataState.length > 0 ?
+            <>
+              <h1>
+                Lista de imagens:
               </h1>
-              <Image className={`${styles.marjoTom} mac:motion:ease-in-out-infinite:[R:-10deg]`} src={marjo_tom} priority={true} alt={'asset no listed images'} />
-            </div>
-          </section >
-      }
-
+              <CardToDownload pictures={dataState} />
+            </>
+            :
+            <NoImagesReceived />
+        }
+      </section>
     </>
   );
 }
